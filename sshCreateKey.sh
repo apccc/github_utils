74a781a7a84537f489a5b2fdafe332ab7c941a68
Abraham P Chaffin <abraham@apc.cc>
2017-06-25 16:05:29 -0700

    Making file executable.

--- sshCreateKey.sh
+++ sshCreateKey.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 K=`echo "$1" | grep -oe '[a-zA-Z0-9_.-]*'`
+U=$2
 F=~/github_keys/"$K"_key
+ssh-keygen -t rsa -f $F -C $U
 if [ ! -f "$F" ];then
+  echo "Key Not Created!"
   exit 1
 fi
-echo "$F"
+echo "Key Created: $F"
 exit 0
