42a9b3d93b615f811c662f83f2c9314ebff17193
Abraham P Chaffin <abraham@apc.cc>
2017-06-25 15:55:04 -0700

    Create repo_pull.sh

---
+++ repo_pull.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+REPOSITORY=`echo "$1" | grep -oe '[a-zA-Z0-9_.-]*'`
+if [ -z "$REPOSITORY" ];then
+  echo "Repository Not Set!"
+  exit 0
+fi
+F=`~/github_utils/sshAgent_getkey.sh "$REPOSITORY"`
+if [ ! -f "$F" ];then
+  echo "Key Not Found : $F!"
+  exit 1
+fi
+eval "$(ssh-agent -s)" > /dev/null 2>&1
+ssh-add $F
+git pull
+~/github_utils/sshAgent_kill.sh
+exit 0
