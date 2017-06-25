5ee6e7899097c8404766a092e5843f4c51dc1882
Abraham P Chaffin <abraham@apc.cc>
2017-06-25 16:23:23 -0700

    Update sshCreateKey.sh

--- sshCreateKey.sh
+++ sshCreateKey.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 K=`echo "$1" | grep -oe '[a-zA-Z0-9_.-]*'`
 U=$2
-F=~/github_keys/"$K"_key
+D=~/github_keys
+F="${D}/${K}"_key
+if [ ! -d "$D" ];then
+  echo "Creating Directory: $D"
+  mkdir -p "$D"
+fi
 ssh-keygen -t rsa -f $F -C $U
 if [ ! -f "$F" ];then
   echo "Key Not Created!"
