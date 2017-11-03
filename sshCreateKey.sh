ecc5e6120143817cbf88cbc083202dd9b0dcaab9
Abraham P Chaffin <abraham@apc.cc>
2017-11-03 14:12:27 -0700

    Update sshCreateKey.sh

--- sshCreateKey.sh
+++ sshCreateKey.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
+#Create SSH Key for GitHub Repository
+
+if [ -z "$1" ];then
+  echo "Error: Repository Name Not Provided!"
+  exit 1
+fi
 K=`echo "$1" | grep -oe '[a-zA-Z0-9_.-]*'`
+
+if [ -z "$2" ];then
+  echo "Error: User Name Not Provided!"
+  exit 1
+fi
 U=$2
+
 D=~/github_keys
 F="${D}/${K}"_key
 if [ ! -d "$D" ];then
